b5cd0e6ecfa34fb18cdc78d1a9d9c56ff900d2a2
Wakame105 <kd1335567@st.kobedenshi.ac.jp>
2025-02-03 14:49:50 +0900

    index.htmlの日本地図から視覚的な場所のリンクの貼り付けと、三角くるくるさせるのと熱海追加

--- js/Kindex.js
+++ js/Kindex.js
@@ -4,6 +4,9 @@ let obj =  $("#O_text_explanation_KM");
 let tab2 = $('#O_text_title_GP');
 let obj2 =  $("#O_text_explanation_GP");
 let lb_PH = $(".lb-tag_a");
+let T_tab = $('#O_title_tab');
+let T_tab2 = $('#O_title_tab2');
+
 tab.on('click',function(){
     // if(obj.hasClass('active'))下のやつでええやん
     // {
@@ -17,10 +20,12 @@ tab.on('click',function(){
     //     obj.addClass('active');
     //     $(this).addClass('active');
     // } 
-    obj.slideToggle();
+  obj.slideToggle();
+  T_tab.toggleClass('open');
 })
 tab2.on('click',function(){
-    obj2.slideToggle();
+  obj2.slideToggle();
+  T_tab2.toggleClass('open');
 })
 
 //==========================Jsonから読み取り=========================
@@ -59,7 +64,6 @@ if( !window.scrollTop ) {
     if($.cookie('percent')){
       $bar.style.width = `${ per }%`;
       let pix_top =  ($body.offsetHeight*per)/100;
-     // $('body,html').animate({scrollTop:pix_top},400,'swing');
     }
     let onscroll = ()=>{
       let $body = document.getElementsByTagName('body')[0];
@@ -72,7 +76,7 @@ if( !window.scrollTop ) {
     window.addEventListener('scroll',onscroll,false);
   }
   setScrollPos();
-  //================scroll to top============================
+  //================上にスムーズスクロール page to top============================
   let button_a =  $('#navi_button');
 $(function(){   
     button_a.on('click',function(){

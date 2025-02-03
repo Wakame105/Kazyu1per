f5dbc627575e7737e9dc85c679dd6dd893bb9587
Wakame105 <kd1335567@st.kobedenshi.ac.jp>
2025-02-03 10:39:20 +0900

    コピーライト表示と下呂温泉追加とガチャの読み込みなおしたとき前回と同じのを表示しない(cookei)

--- js/index.js
+++ js/index.js
@@ -5,11 +5,18 @@ let Gc_txt1 = $("#Gacha_text1");
 let Gc_txt2 = $("#Gacha_text2");
 let Gc_ttl = $('#Gacha_title');
 let Gc_img = $('#Gacha_img');
+let GC_CPR = $('#maru_C');
+let GC_CPR_txt = $('#maru_C_text');
 var click_flg=false;
 var data_num = 0;
 var random_num = 0;
-var now_num = 0;
+var now_num=0;
 var json_data;
+
+if($.cookie('before_num')){
+    now_num = $.cookie('before_num');
+}
+
 Gc_btn.on('click' ,function(){
     if(!click_flg){
     Gc_cnt.slideDown();
@@ -35,7 +42,6 @@ $(function(){
 
 function change_obj(){
     //データの数からランダムでとる。
-    
     GetRandomNumber();
     
     let obj = json_data[random_num];
@@ -44,6 +50,20 @@ function change_obj(){
     Gc_txt1.text(obj.text1);
     Gc_txt2.text(obj.text2);
     Gc_img.attr('src',obj.img_url);
+    if(obj.CPR_Type){
+        if(obj.CPR_Type=='T'){
+            GC_CPR.text("提供:");
+        }
+        else if(obj.CPR_Type=='C'){
+            GC_CPR.text("©");
+        }
+        else if(obj.CPR_Type=='S'){
+            GC_CPR.text("撮影:");
+        }
+        GC_CPR_txt.text(obj.CPR);
+        GC_CPR_txt.attr('href',obj.CPR_url)
+    }
+    
 }
 
 function GetRandomNumber(){
@@ -52,5 +72,5 @@ function GetRandomNumber(){
     }
     while(now_num === random_num);
     now_num=random_num;
-    console.log(random_num);
+    $.cookie('before_num',`${ now_num }`,{expires:1});
 }
